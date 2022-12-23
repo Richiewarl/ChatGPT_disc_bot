d95f293e79043605376c0d4637cfc536f294e5a5
Richiewarl <leechyesoon@gmail.com>
2022-12-23 02:59:00 +0000

    Switch to discord-ext command and make basic request to OpenAI API

--- main.py
+++ main.py
@@ -1,28 +1,49 @@
 import discord
+from discord.ext import commands
+import openai
 from dotenv import load_dotenv
 import os
 
 # loading .env file
 load_dotenv()
 
-# discord object
+# discord objects
 intents = discord.Intents.all()
-client = discord.Client(command_prefix='~', intents=intents)
+bot = commands.Bot(command_prefix='~', intents=intents)
 
-# events
-@client.event
+# openai objects
+openai.api_key = os.getenv('OPENAI_TOKEN')
+
+# test outputs
+
+
+# register on start
+@bot.event
 async def on_ready():
-  print("Hi! We've logged in as {0.user}".format(client))
+    print("Hi! We've logged in as {0.user}".format(bot))
 
+# commands
+@bot.command()
+async def ping(ctx):
+    await ctx.channel.send("pong :ping_pong:")
 
-@client.event
-async def on_message(message):
-  # ignore own messages
-  if (message.author == client.user):
-    return
+@bot.command()
+async def models(ctx):
+    await ctx.channel.send(f" \
+        The OpenAI API currently has {len(openai.Model.list().data)} available model from the GPT-3, Codex and Content Filter family. They each have their own capabilities and strengths. I primarily use models from the GPT-3 family for natural language processing which allows me to answer your questions. The main GPT-3 models are: \n \t1. Davinci - <https://beta.openai.com/docs/models/davinci> \n \t2. Curie - <https://beta.openai.com/docs/models/curie> \n \t3. Babbage - <https://beta.openai.com/docs/models/babbage> \n \t4. Ada - <https://beta.openai.com/docs/models/ada> \n To learn more, visit https://beta.openai.com/docs/models/overview. \
+        ")
 
-  if message.content.startswith('ask'):
-    await message.channel.send("SUCCESS")
+@bot.command()
+async def completion(ctx, arg):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=arg,
+        max_tokens=3900,
+        temperature=1
+        )
+    print(response)
+    await ctx.channel.send(response.choices[0].text)
+
 
 # exe
-client.run(os.getenv('BOT_TOKEN'))
+bot.run(os.getenv('BOT_TOKEN'))
